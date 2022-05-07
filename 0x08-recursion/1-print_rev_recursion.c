af00919b1f8fdbfef663022d486487be59854cbc
subong360 <subong271@gmail.com>
2022-05-06 17:25:20 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,37 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
+ * _print_rev_recursion - prints a string in reverse.
  * @s: string
- * Return: the length of a string.
+ * Return: no return.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
-
-
-Task 3 - 3-factorial.c
-
-Ans:
-
-#include "main.h"
-
-/**
- * factorial - returns the factorial of a given number.
- * @n: input number.
- * Return: factorial of the number.
- */
-int factorial(int n)
-{
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (1);
-	else
-		return (n * factorial(n - 1));
-}
-
