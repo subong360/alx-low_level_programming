060f2a88d643dc17025ce9835b7976ae484f5539
subong360 <subong271@gmail.com>
2022-06-02 14:54:52 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+es (13 sloc)  384 Bytes
+   
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -13,4 +15,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
