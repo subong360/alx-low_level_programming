b01dd2e6bb3b82382f041f4e02866fa1031a3ce5
subong360 <subong271@gmail.com>
2022-06-03 09:39:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -9,25 +8,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+ssize_t file, fread, fwrite;
+char *totalSize;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+totalSize = malloc(sizeof(char) * letters);
+if (totalSize == NULL)
+return (0);
+if (filename == NULL)
+return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+file = open(filename, O_RDONLY);
+if (file == -1)
+return (0);
+fread = read(file, totalSize, letters);
+if (fread == -1)
+return (0);
+   
+fwrite = write(STDOUT_FILENO, totalSize, fread);
+if (fwrite == -1)
+return (0);
+close(file);
+free(totalSize);
+return (fwrite);
 }
